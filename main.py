9b61b4d350d574cef4b20e444360098a35f0ae1a
ViniciusCarvalhoLima <128156331+ViniciusCarvalhoLima@users.noreply.github.com>
2023-05-28 15:30:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 def is_palindrome(string):
     limpa_string = string.lower().replace(" ", "").replace(".", "").replace(",", "").replace("!", "")
     return limpa_string == limpa_string[::-1]
-
-string = str(input("cheque se é palindromo:\n"))
